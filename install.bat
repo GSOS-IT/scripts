38fc0a447f309b6e4026521c390ebd25fdcf3ea5
GSOS-IT <jmah@goldenstateortho.com>
2025-07-16 11:52:08 -0700

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,1 +1,1 @@
-msiexec.exe /i "NetExtender-x64-10.2.341.msi" ALLUSERS=1 /qn /norestart /log output.log MODE=onlyone SERVER=99.64.157.65:4434 DOMAIN=LocalDomain EDITABLE=FALSE ALLUSERS=2
+msiexec.exe /i "NetExtender-x64-10.2.341.msi" /qn /norestart /log output.log MODE=onlyone SERVER=99.64.157.65:4434 DOMAIN=LocalDomain EDITABLE=FALSE ALLUSERS=2
